b77025659773391a5d6c2b3a73f637228b9f90d2
Junior Njagi <kungurunjagi@gmail.com>
2023-12-30 23:25:10 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+const num1=31
+const num2=2
+
+var multiply = num1 * num2
+
+console.log(multiply) 
+
+
+let random = Math.floor(Math.random() >0 ) ;
+
+console.log(random)
+
+let num3 = 13
+let num4 = 9
+let mod = num3 % num4;
+console.log(mod)
+
+
+let max= Math.max(15,16,17,19,20)
+console.log(max)
